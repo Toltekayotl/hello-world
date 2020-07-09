bd92620117805c34b9bc646c94356cdce4b2d988
Toltekayotl <32440528+Toltekayotl@users.noreply.github.com>
2020-07-09 21:35:01 +0300

    Update bot_config.py

--- bot_config.py
+++ bot_config.py
@@ -3,10 +3,10 @@ DEBUG = True  # or False
 OPERATIONS = {'operations': [{'timestamp': 0}]}
 
 # id or @name
-TELEGRAM = {'token': '1381072570:AAFEifq6hs1ppgF2h7ACi4sL9uMVlgYrrI0',
-            'user_id': '@Toltek6',
+TELEGRAM = {'token': '',
+            'user_id': '',
             'group_id': '',
-            'channel_id': '@keep_tbtc'}
+            'channel_id': ''}
             
 # https://ethplorer.io/wallet/#api
 API_KEY = {"apiKey": 'EK-7wrup-LJdaYw3-G9hEy'}
